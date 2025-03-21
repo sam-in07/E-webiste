59ad5753e8bf948fabc1a8dc79073cb2414ead71
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-21 23:44:06 +0600

    leloooo

--- webchiteeee/webchiteeee/settings.py
+++ webchiteeee/webchiteeee/settings.py
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'webchiteeee/static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
