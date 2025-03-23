07915cd197ade9ee64807d6f243a67d3a2e7fc1f
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-23 23:20:49 +0600

    hehehe

---
+++ webchiteeee/catagory/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Catagory
+
+admin.site.register(Catagory)
+
+# akahne amra models ar admin creat korsi just
---
+++ webchiteeee/catagory/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+#  catagory name ta boro hater hobe
+class Catagory(models.Model) :
+    catagory_name = models.CharField(max_length =50 , unique=True)  # A character field to store the name of the category
+    slug = models.CharField(max_length =100 , unique=True)     # A character field to store the unique slug (max length 100), used for creating SEO-friendly URLs
+
+    # The
+    # slug is often
+    # used
+    # to
+    # create
+    # user - friendly
+    # URLs
+    # based
+    # on
+    # the
+    # category
+    # name, so
+    # it
+    # 's common to make it unique.
+
+    # A text field to store a description of the category, with a max length of 255 characters
+    # It is optional, so it can be left blank
+
+    desciption = models.TextField(max_length=255 , blank=True)
+    # An image field to store the category image
+    # Files are uploaded to the 'photos/catagories' directory
+    # This field is also optional, so it can be left blank
+    cat_image = models.ImageField(upload_to= 'photos/catagories' , blank=True )
+    # The
+    # cat_image
+    # field
+    # stores
+    # an
+    # image
+    # file.Ensure
+    # that
+    # you
+    # 've configured your Django app to serve and store media files correctly.
+# The string representation of the model instance (what will be displayed in admin or other places)
+    # In this case, it returns the category name when the object is called as a string
+def __str__(self) :
+    return  self.catagory_name
