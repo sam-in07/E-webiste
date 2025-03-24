221d7a41db638412cd5b12a9e7937ddac993b884
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-24 19:46:10 +0600

    commit

---
+++ webchiteeee/accounts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
+# Create your models here.
+
+
+class Account(AbstractBaseUser )  :
+    first_name = models.CharField(max_length=50)
+    last_name  = models.CharField(max_length=50)
+    username     = models.CharField(max_length=50, unique=True)
+    email      = models.CharField(max_length=50 , unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    #also agulaow dorker
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login  = models.DateTimeField(auto_now=True)
+    is_admin  = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+
+
+
+
--- webchiteeee/catagory/models.py
+++ webchiteeee/catagory/models.py
@@ -6,20 +6,9 @@ class Catagory(models.Model) :
     catagory_name = models.CharField(max_length =50 , unique=True)  # A character field to store the name of the category
     slug = models.CharField(max_length =100 , unique=True)     # A character field to store the unique slug (max length 100), used for creating SEO-friendly URLs
 
-    # The
-    # slug is often
-    # used
-    # to
-    # create
-    # user - friendly
-    # URLs
-    # based
-    # on
-    # the
-    # category
-    # name, so
-    # it
-    # 's common to make it unique.
+    # A "slug" is   way    of    generating    a    valid    URL, generally    using    data    already  obtained.For    instance, a    slug
+    # uses    the    title   of    an    article to    generate    a    URL.I   advise    to  generate    the    slug    by
+    # means   of   a   function, given   the   title( or another  piece  of data), rather  than  setting  it  manually
 
     # A text field to store a description of the category, with a max length of 255 characters
     # It is optional, so it can be left blank
@@ -41,5 +30,11 @@ class Catagory(models.Model) :
     # 've configured your Django app to serve and store media files correctly.
 # The string representation of the model instance (what will be displayed in admin or other places)
     # In this case, it returns the category name when the object is called as a string
+
+    class Meta:
+        verbose_name = 'Catagory'
+        verbose_name_plural = 'Catagories'
+
+
 def __str__(self) :
     return  self.catagory_name
