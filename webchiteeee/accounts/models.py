27a1f73035962dfee22eed65faf46d7cfcf2f99c
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-03-26 22:29:16 +0600

    commit

--- webchiteeee/accounts/models.py
+++ webchiteeee/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
 # Create your models here.
-
+#    account model code
 
 class Account(AbstractBaseUser )  :
     first_name = models.CharField(max_length=50)
@@ -17,6 +17,20 @@ class Account(AbstractBaseUser )  :
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username' , 'first_name', 'last_name']
+
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None) :
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+
 
 
 
